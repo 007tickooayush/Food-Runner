4c0afc8f091bf9f1af7a16054b00595324e21bb8
007tickooayush <48850318+007tickooayush@users.noreply.github.com>
2020-12-14 10:42:29 +0530

    Delete Favourite.kt

--- foodrunner/model/Favourite.kt
+++
@@ -1,9 +0,0 @@
-package com.reazon.foodrunner.model
-
-data class Favourite (
-    var id:String? = null,
-    var name:String? = null,
-    var rating:String? = null,
-    var cost_for_one:String? = null,
-    var image_url:String? = null
-)
