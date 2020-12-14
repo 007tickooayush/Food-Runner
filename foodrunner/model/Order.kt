c0c241779fb2507450f6e8fb63391c487f9c679c
007tickooayush <48850318+007tickooayush@users.noreply.github.com>
2020-12-14 10:42:46 +0530

    Delete Order.kt

--- foodrunner/model/Order.kt
+++
@@ -1,8 +0,0 @@
-package com.reazon.foodrunner.model
-data class Order (
-    val id:Int? = null,
-    val item:String?= null,
-    val cost:Int?= null,
-    val placed:Boolean?= false,
-    val time:String? = ""
-)
